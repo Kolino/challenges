be3293b346fa32d4a2ad91c4a3737f948412a4ff
Keelan Black <keelan.black1@gmail.com>
2023-06-05 09:52:28 +1000

    adds JS folder and types challenge within it

---
+++ keelan/javascript/types/types.js
@@ -0,0 +1,13 @@
+console.log(typeof 15); // number
+console.log(typeof 5.5); // number
+console.log(typeof NaN); // number
+console.log(typeof 'hello'); // string
+console.log(typeof true); // boolean
+console.log(typeof (1 != 2)); // boolean
+console.log(typeof ('hamburger' + 's')); // string
+console.log(typeof ('hamburgers' - 's')); // number
+console.log(typeof ('1' + '3')); // string
+console.log(typeof ('1' - '3')); // number
+console.log(typeof ('johnny' + 5)); // string
+console.log(typeof ('johnny' - 5)); // number
+console.log(typeof (99 * 'luftballons')); // number
