f57e229612d26c822f6296c5ef58d5a669e309c4
Keelan Black <keelan.black1@gmail.com>
2023-06-05 12:34:27 +1000

    adds JS operators challenge

---
+++ keelan/javascript/operators/script.js
@@ -0,0 +1,20 @@
+let myAge = 27;
+myAge += 10;
+console.log(myAge);
+
+myAge -= 7;
+console.log(myAge);
+
+myAge *= 11;
+console.log(myAge);
+
+// Part 2
+const numEarthDays = prompt("Enter the number of earth days: ");
+// Number of Earth days the moon takes to rotate once
+const numEarthDaysPerRotation = 27;
+console.log(numEarthDays / numEarthDaysPerRotation);
+
+// Part 3
+// Number of Earth days the moon takes to orbit the Earth once
+const numEarthDaysPerOrbit = 27;
+console.log(numEarthDays / numEarthDaysPerOrbit);
